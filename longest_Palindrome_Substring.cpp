61439854ba8cdd847510dca7bb0be873d899f5bf
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-07-14 23:57:40 +0600

    Update longest_Palindrome_Substring.cpp

--- longest_Palindrome_Substring.cpp
+++ longest_Palindrome_Substring.cpp
@@ -1,71 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define  CIN            ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define  ll             long long int
-
-string s1;
-string convertString(const string &s)
+string text;
+int min(int a, int b)
 {
-    string newString;
-    for(ll i=0; i<s.size(); i++)
-    {
-        newString+="#"+s.substr(i,1);
-    }
-    newString+="#";
-    return newString;
+	int res = a;
+	if(b < a)
+		res = b;
+	return res;
 }
 
-string lps(const string &s)
+string findLongestPalindromicString()
 {
-    string str = convertString(s);
-    ll c=0,r=0;
-    ll len = str.size();
-    ll LPS[len+2]= {0};
-    for(ll i=0; i<len; i++)
-    {
-        ll mirror =2*c-1;
-        if(r>i)
-        {
-            LPS[i]=min(r-i,LPS[mirror]);
-        }
-        ll a=i+(1+LPS[i]);
-        ll b=i-(1+LPS[i]);
-        while(a<len&&b>=0&&str[a]==str[b])
-        {
-            LPS[i]++;
-            a++;
-            b--;
-        }
-        if(i+LPS[i]>r)
-        {
-            c=i;
-            r=i+LPS[i];
-        }
-    }
+	int N = text.size();
+	if(N == 0)  return "";
+	N = 2*N + 1;
+	int L[N];
+	L[0] = L[1]=0;
+	int C=1,R=2,i=0,iMirror,mx=0,center=0,start=-1,endd=-1,diff=-1;
+	for (i = 2; i < N; i++)
+	{
+		iMirror = 2*C-i;
+		L[i] = 0;
+		diff = R - i;
+		if(diff > 0) L[i] = min(L[iMirror], diff);
 
-    ll maxlen=0,indx=0;
-    for (ll i = 0; i < str.size() ; i++)
-    {
-        if (LPS[i] > maxlen)
-        {
-            maxlen = LPS[i];
-            indx = i;
-        }
-    }
-    return s.substr((indx-maxlen)/2,maxlen);
+		while(((i+L[i])<N &&(i-L[i])>0)&&(((i+L[i]+1)%2==0)||(text[(i+L[i]+1)/2]==text[(i-L[i]-1)/2]))) L[i]++;
+
+		if(L[i]>mx)
+		{
+			mx = L[i];
+			center = i;
+		}
+		if (i+L[i]>R)
+		{
+			C = i;
+			R = i+L[i];
+		}
+	}
+	start = (center-mx)/2;
+	return text.substr(start,mx);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    CIN;
-    ll  a,b,c,d,e,f,i,j,k,cnt1=0,cnt2=0,sum1=0,sum2=0,chk1=0,chk2=0,T;
-    cin>> T ;
-    while(T--)
-    {
-        cin >> s1 ;
-        cout<<lps(s1)<<endl;
-    }
-    return 0 ;
+    int a;
+	cin >>a>> text ;
+	cout<<findLongestPalindromicString()<<endl;
+	return 0;
 }
-//https://medium.com/প্রোগ্রামিং-পাতা/লংগেস্ট-প্যালিনড্রোমিক-সাবস্ট্রিং-manachers-algorithm-3c4d6880c905
